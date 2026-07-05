0ef749cb8ce8477b5a29984a54e40858e14fd207
sandymohsen2019-ui <sandy.mohsen2019@gmail.com>
2026-07-05 22:23:54 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,40 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwYxiDhIhw-t5bYSSrEP0nmDRHoTDKNbFVk_hlEqkwQUTYhdnl1Th_qhh3NkeQLtfrN/exec";
 
+async function recordAttendance(qrValue, scannerName) {
+
+    try {
+
+        const response = await fetch(API_URL, {
+
+            method: "POST",
+
+            redirect: "follow",
+
+            headers: {
+                "Content-Type": "text/plain;charset=utf-8"
+            },
+
+            body: JSON.stringify({
+                qr: qrValue,
+                scanner: scannerName
+            })
+
+        });
+
+        const result = await response.json();
+
+        return result;
+
+    }
+    catch (err) {
+
+        return {
+
+            success: false,
+            message: err.message
+
+        };
+
+    }
+
+}
