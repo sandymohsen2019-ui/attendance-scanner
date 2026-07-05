1a344fd88359e53eed2aa47b6785b35fa5013171
sandymohsen2019-ui <sandy.mohsen2019@gmail.com>
2026-07-05 21:29:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
+document
+.getElementById("startBtn")
+.addEventListener("click",startScanner);
 
+let scanner;
+
+function startScanner(){
+
+    const scannerName=document
+    .getElementById("scannerSelect")
+    .value;
+
+    if(scannerName==""){
+
+        alert("Please select a scanner.");
+
+        return;
+
+    }
+
+    localStorage.setItem(
+        "scanner",
+        scannerName
+    );
+
+    document.getElementById("message")
+    .innerHTML="Opening Camera...";
+
+    scanner=new Html5Qrcode("reader");
+
+    Html5Qrcode.getCameras()
+
+    .then(cameras=>{
+
+        if(cameras.length==0){
+
+            alert("No Camera Found");
+
+            return;
+
+        }
+
+        scanner.start(
+
+            cameras[0].id,
+
+            {
+                fps:10,
+                qrbox:250
+            },
+
+            onScanSuccess
+
+        );
+
+    })
+
+    .catch(err=>{
+
+        alert(err);
+
+    });
+
+}
+
+function onScanSuccess(decodedText){
+
+    document.getElementById("message")
+    .innerHTML=decodedText;
+
+}
